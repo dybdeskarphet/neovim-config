93e49d98919b3386e38d7ad86bf09e41aa847118
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-05-26 02:22:22 +0300

    Remove todotxt support since I don't use it anymore

--- plugin/06_tools.lua
+++ plugin/06_tools.lua
@@ -1,27 +1,13 @@
 -- Initialize plugins {{{
-add({ gh("lervag/vimtex"), gh("phrmendes/todotxt.nvim") })
+add({
+	gh("lervag/vimtex"),
+})
 -- }}}
 
 -- vimtex {{{
 g.vimtex_view_method = "zathura"
 g.vimtex_view_general_viewer = "zathura"
 g.vimtex_compiler_method = "latexrun"
--- }}}
-
--- todotxt {{{
-require("todotxt").setup({
-	todotxt = vim.env.HOME .. "/doc/current.todo.txt",
-	donetxt = vim.env.HOME .. "/doc/done.todo.txt",
-	ghost_text = {
-		enable = true,
-		mappings = {
-			["(A)"] = "important, urgent",
-			["(B)"] = "important, not urgent",
-			["(C)"] = "not important, urgent",
-			["(D)"] = "not important, not urgent",
-		},
-	},
-})
 -- }}}
 
 later(function()
