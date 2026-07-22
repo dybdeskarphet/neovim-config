2b88b8b14dd48af31fab22a3556b5aa06cf154fc
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-07-22 08:18:26 +0300

    Increase the formatting timeout

--- plugin/06_lsp.lua
+++ plugin/06_lsp.lua
@@ -190,7 +190,7 @@ later(function()
 			if g.disable_autoformat or vim.b[bufnr].disable_autoformat then
 				return
 			end
-			return { timeout_ms = 500, lsp_format = "fallback" }
+			return { timeout_ms = 2000, lsp_format = "fallback" }
 		end,
 	})
 	-- }}}
